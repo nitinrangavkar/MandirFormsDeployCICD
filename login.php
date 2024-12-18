2f9d9a386f81253d5ae5e56fc8e5615f8e605247
nitinrangavkar <48552029+nitinrangavkar@users.noreply.github.com>
2024-12-18 19:32:01 +0530

    Update login.php

--- login.php
+++ login.php
@@ -162,7 +162,7 @@ if (isset($_POST['login_user'])) {
 
                   <div class="pt-4 pb-2">
                     <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                    <p class="text-center small">Enter your Username & password to login - Update 2</p>
+                    <p class="text-center small">Enter your Username & password to login - Update 3</p>
                     <?php if (isset($error_msg)) { ?>
                         <p style="color: red;"><?php echo $error_msg; ?></p>
                     <?php } ?>
